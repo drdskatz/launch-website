34aa5840d3dd575f9f690d1b375c8c99c8bf875b
drdskatz <katz@houston-therapy.com>
2026-06-16 16:00:20 -0500

    Update astro.config.mjs

--- launch-iop/astro.config.mjs
+++ launch-iop/astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://launch-website-ebon.vercel.app',
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   integrations: [react(), markdoc(), keystatic(), sitemap()],
 });

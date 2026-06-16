f100da7c48e68c2b0e855049fed97544ac3cdea5
drdskatz <katz@houston-therapy.com>
2026-06-16 15:56:10 -0500

    Update astro.config.mjs

--- launch-iop/astro.config.mjs
+++ launch-iop/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@vercel/astro';
+import vercel from '@astrojs/vercel';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://launch-website-ebon.vercel.app',
   output: 'hybrid',
   adapter: vercel(),
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), sitemap()],
 });

6832eec8635432b1f26661194ca675670d808241
drdskatz <katz@houston-therapy.com>
2026-06-16 15:39:40 -0500

    Update astro.config.mjs

--- launch-iop/astro.config.mjs
+++ launch-iop/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import vercel from '@vercel/astro';
+import keystatic from '@keystatic/astro';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
 
 export default defineConfig({
-  site: 'https://www.launchiop.com',
-  integrations: [sitemap()],
+  output: 'hybrid',
+  adapter: vercel(),
+  integrations: [react(), markdoc(), keystatic()],
 });
